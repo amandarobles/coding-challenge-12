ba726df34c339e6c4dfbd3ef767665fa702b0362
Amanda Robles <amandarobles@usf.edu>
2025-02-27 16:31:06 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -5,3 +5,11 @@ revenueCard.setAttribute("class", "metric-card"); //assigning a class
 revenueCard.setAttribute("id", "revenueCard");//assigning an id to the new element
 revenueCard.innerHTML = "<h2>Revenue</h2><p>$0</p"; //populating the card with title and a placeholder value
 dashboard.appendChild(revenueCard); //appending the new metric card to the dashboard
+
+// task 2: Updating Dashboard Metrics- NodeLists and Arrays
+const metricCards = document.querySelectorAll(".metric-card"); //selecting elements with the class "metric-card"
+const metricCardsArray = Array.from(metricCards); //convert Nodelist to an array
+metricCardsArray.forEach(card => { //updating card's inner text and style
+    card.style.backgroundColor = "lighblue"; //changing the background color
+    card.innerHTML += "- Updated"; //appending to the current text
+})
